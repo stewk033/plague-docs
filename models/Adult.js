ff325f02e45fd86bf33a8a7e89cdc2be13ab1a2f
insomniKat <ru3ykat@gmail.com>
2022-01-27 21:20:17 -0500

    models and routes server setup

---
+++ models/Adult.js
@@ -0,0 +1,42 @@
+const { Schema, model } = require('mongoose');
+
+const AdultSchema = new Schema (
+    {
+        firstName: {
+            type: String,
+            unique: true,
+            required: 'Please enter your first name',
+            trim: true
+        },
+        lastName: {
+            type: String,
+            unique: true,
+            required: 'Please enter your first name',
+            trim: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            trim: true,
+            // regex for basic email format including @
+            match: [/.+\@.+\..+/, 'Please input a valid email address']
+        },
+        phoneNumber: {
+            type: String,
+            require: true,
+            match: [/^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/, 'Please enter a valid phone number']
+        }
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+
+    }
+);
+
+const Adult = model('Adult', AdultSchema);
+
+module.exports = Adult;
