d4f3397f7c043d67815f009305396f7a345afebd
chl850405 <christimarchetti@gmail.com>
2022-01-27 20:26:46 -0500

    components and pages

---
+++ client/src/components/Settings.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Dropdown } from 'semantic-ui-react'
+
+function Settings() {
+  return(
+    <Dropdown
+    icon='cog'
+    floating
+    labeled
+    button
+    className='icon'
+  >
+    <Dropdown.Menu>
+      <Dropdown.Header icon='tags' content='Settings' />
+      <Dropdown.Divider />
+      <Dropdown.Item icon='attention' text='Edit Account' />
+      <Dropdown.Item icon='comment' text='Add Family Member' />
+      <Dropdown.Item icon='conversation' text='Log Out' />
+    </Dropdown.Menu>
+  </Dropdown>
+)
+}
+
+export default Settings
---
+++ client/src/components/Submit.js
@@ -0,0 +1,11 @@
+import React from 'react'
+// import the minified CSS file
+import 'semantic-ui-css/semantic.min.css'
+import { Button } from 'semantic-ui-react'
+
+function Submit(props){
+    //button with loading feature when clicked
+    <Button type='submit' basic loading {...props} />
+}
+
+export default Submit
