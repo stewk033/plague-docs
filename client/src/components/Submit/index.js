48643c48b700459ab0cedcb3fe4aa925c90b6943
chl850405 <christimarchetti@gmail.com>
2022-01-30 19:02:57 -0500

    add return in submit

--- client/src/components/Submit/index.js
+++ client/src/components/Submit/index.js
@@ -1,11 +1,11 @@
 import React from 'react'
-// import the minified CSS file
-import 'semantic-ui-css/semantic.min.css'
 import { Button } from 'semantic-ui-react'
 
 function Submit(props){
+    return(
     //button with loading feature when clicked
     <Button type='submit' basic loading {...props} />
+    )
 }
 
 export default Submit
