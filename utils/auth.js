fb253f7c37c430c60a35b04f3a536d0727e5ffa8
stewk033 <kevin.c.stewart04@gmail.com>
2022-02-01 14:59:50 -0500

    jwt

---
+++ utils/auth.js
@@ -0,0 +1,11 @@
+const jwt = require('jsonwebtoken');
+
+const secret = 'mysecret';
+
+module.exports = {
+  signToken: function({ username, email, _id }) {
+    const payload = { username, email, _id };
+
+    return jwt.sign({ data: payload }, secret);
+  }
+};
