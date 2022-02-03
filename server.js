19f7310a136781e1675219251f1e6e6e5ae89575
stewk033 <kevin.c.stewart04@gmail.com>
2022-02-03 16:01:36 -0500

    update to typeDef

--- server.js
+++ server.js
@@ -1,47 +1,42 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const { ApolloServer } = require("apollo-server-express");
+// const { ApolloServer } = require("apollo-server-express");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// const { graphqlHTTP } = require("express-graphql");
-const { typeDefs, resolvers } = require("./server/Schema/index.js");
-const { authMiddleware } = require("./utils/auth");
+const { graphqlHTTP } = require("express-graphql");
+const db = require("./config/connection");
+const schema = require("./server/Schema/index.js");
+// const { authMiddleware } = require("./utils/auth");
 const cors = require("cors");
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-// app.use(
-//   "/graphql",
-//   graphqlHTTP({
-//     schema,
-//     graphiql: true,
-//   })
-// );
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    graphiql: true,
+  })
+);
 
-const startServer = async () => {
-  const server = new ApolloServer({
-    typeDefs,
-    resolvers,
-    context: authMiddleware,
-  });
-  await server.start();
-  server.applyMiddleware({ app });
-  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
-};
+// const startServer = async () => {
+//   const server = new ApolloServer({
+//     typeDefs,
+//     resolvers,
+//     context: authMiddleware,
+//   });
+//   await server.start();
+//   server.applyMiddleware({ app });
+//   console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+// };
 
-startServer();
+// startServer();
 
 app.use(require("./routes"));
 
-mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost:27017/plague-docs",
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  }
-);
-
-app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
+db.once("open", () => {
+  app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
+});
--- server/Schema/typeDefs.js
+++ server/Schema/typeDefs.js
@@ -1,50 +1,70 @@
-const { gql } = require('apollo-server-express');
+const { gql } = require("apollo-server-express");
 
 const typeDefs = gql`
-  type User {
+  type Adult {
     _id: ID
-    username: String
+    firstName: String
+    lastName: String
+    gender: String
     email: String
-    friendCount: Int
-    thoughts: [Thought]
-    friends: [User]
+    phoneNumber: String
+    birthDate: String
+    createdAt: String
+    updatedAt: String
   }
 
-  type Thought {
+  type AdultVaxCard {
     _id: ID
-    thoughtText: String
+    vaxType: String
+    vaxDate: String
+    facilityLocation: String
+    lotNum: String
+    certNum: String
+    dose: String
     createdAt: String
-    username: String
-    reactionCount: Int
-    reactions: [Reaction]
+    updatedAt: String
   }
 
-  type Reaction {
+  type Child {
     _id: ID
-    reactionBody: String
+    firstName: String
+    lastName: String
+    gender: String
+    email: String
+    phoneNumber: String
+    birthDate: String
     createdAt: String
-    username: String
+    updatedAt: String
+  }
+
+  type ChildVaxCard {
+    _id: ID
+    vaxType: String
+    vaxDate: String
+    facilityLocation: String
+    lotNum: String
+    certNum: String
+    dose: String
+    createdAt: String
+    updatedAt: String
   }
 
   type Auth {
     token: ID!
-    user: User
+    user: Adult
   }
 
   type Query {
-    me: User
-    users: [User]
-    user(username: String!): User
-    thoughts(username: String): [Thought]
-    thought(_id: ID!): Thought
+    me: Adult
+    users: [Adult]
+    user(username: String!): Adult
   }
 
   type Mutation {
-    login(email: String!, password: String!): Auth
     addUser(username: String!, email: String!, password: String!): Auth
-    addThought(thoughtText: String!): Thought
-    addReaction(thoughtId: ID!, reactionBody: String!): Thought
-    addFriend(friendId: ID!): User
+    login(email: String!, password: String!): Auth
+    saveVaxCard(vaxCardData: String!): AdultVaxCard
+    removeVaxCard(vaxCardId: ID!): AdultVaxCard
   }
 `;
 
