4ea47d6c186027c98a5cf7fd68d2881109ae3830
stewk033 <kevin.c.stewart04@gmail.com>
2022-02-03 14:46:20 -0500

    typeDefs error

--- server.js
+++ server.js
@@ -1,32 +1,47 @@
-const express = require('express');
-const mongoose = require('mongoose');
+const express = require("express");
+const mongoose = require("mongoose");
+const { ApolloServer } = require("apollo-server-express");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const { graphqlHTTP } = require("express-graphql");
-const schema = require("./server/Schema/index.js");
+// const { graphqlHTTP } = require("express-graphql");
+const { typeDefs, resolvers } = require("./server/Schema/index.js");
+const { authMiddleware } = require("./utils/auth");
 const cors = require("cors");
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-// app.use(express.static('public'));
-app.use(
-  "/graphql",
-  graphqlHTTP({
-    schema,
-    graphiql: true,
-  })
-);
+// app.use(
+//   "/graphql",
+//   graphqlHTTP({
+//     schema,
+//     graphiql: true,
+//   })
+// );
 
-app.use(require('./routes'));
+const startServer = async () => {
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: authMiddleware,
+  });
+  await server.start();
+  server.applyMiddleware({ app });
+  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+};
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/plague-docs', {
+startServer();
+
+app.use(require("./routes"));
+
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost:27017/plague-docs",
+  {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-});
-
-mongoose.set('debug', true);
+  }
+);
 
 app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
--- server/Schema/index.js
+++ server/Schema/index.js
@@ -1,8 +1,4 @@
+const typeDefs = require("./typeDefs/index");
 const resolvers = require("./resolvers");
-// const AdultCardType = require("./typeDefs/AdultCardType");
-// const AdultUser = require("./typeDefs/AdultUser");
-// const ChildCardType = require("./typeDefs/ChildCardType");
-// const ChildUser = require("./typeDefs/ChildUser");
-// const typeDefs = { AdultCardType, AdultUser, ChildCardType, ChildUser };
 
-module.exports = { resolvers };
+module.exports = { typeDefs, resolvers };
